54b22fcccdfd9ed500c37b3906e9591599469809
Chen <lerx98@gmail.com>
2022-04-06 22:16:19 +0800

    Implement QueryDetails class and parse query logic

---
+++ hw4/query.py
@@ -0,0 +1,70 @@
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
+class QueryDetails:
+    """
+    Does pre-processing, determines type of query, and put the respective information
+    into variables.
+
+    Args:
+        query         (str)  : String form of the whole query
+        relevant_docs (list) : List of all the given relevant doc Ids
+
+    Variables:
+        type                : the type of querym which is either a boolean or free-text
+        terms (free-text)   : all the terms in the free-text query
+        lhs (boolean)       : term on the LHS of the AND query
+        rhs (boolean)       : term on the RHS of the AND query
+    """
+    def __init__(self, query, relevant_docs):
+        #? Edge case: 2 phrasal queries -> "apple bottom" AND "hello world" <- is this possible?
+
+        self.type = "free-text"
+
+        # Word_tokenization
+        tokens = word_tokenize(query)
+
+        # Check if its boolean query
+        for i in range(len(tokens)):
+            if tokens[i] == "AND":
+                self.type = "boolean"
+                booleanIndex = i
+                break
+
+        # Stemming + Case-folding
+        ps = PorterStemmer()
+        for i in range(len(tokens)):
+            tokens[i] = tokens[i].lower()
+            tokens[i] = ps.stem(tokens[i])
+
+        print("tokens: ", tokens)
+
+        # TODO : Handle relevant_docs to do pseudo Relevant Feedback
+        self.relevant_docs = relevant_docs
+        
+        # Check for quotations for phrasal queries
+        phrasal_query = []
+        phrasal_start_index = -1
+        phrasal_end_index = -1
+        for i in range(len(tokens)):
+            if tokens[i] == "``" and phrasal_start_index == -1: # start quotation
+                phrasal_start_index = i + 1
+            elif tokens[i] == "''" and phrasal_end_index == -1: # end quotation
+                phrasal_end_index = i
+                phrasal_query = tokens[phrasal_start_index : phrasal_end_index]
+                break
+                
+        if self.type == "free-text":
+            self.terms = tokens
+        else:
+            # check if there's phrasal_query, and on which side
+            if phrasal_start_index != -1:
+                if phrasal_start_index == 1:
+                    self.lhs = phrasal_query
+                    self.rhs = tokens[booleanIndex + 1: len(tokens)]
+                elif phrasal_start_index > booleanIndex:
+                    self.rhs = phrasal_query
+                    self.lhs = tokens[0: booleanIndex]
+            else:
+                self.lhs = tokens[0: booleanIndex]
+                self.rhs = tokens[booleanIndex + 1: len(tokens)]
--- hw4/search.py
+++ hw4/search.py
@@ -3,6 +3,8 @@ import pickle
 import sys
 import getopt
 
+from query import QueryDetails
+
 def get_posting_list(dictionary, postings, term):
         """
         Retrieves posting list of term from posting file
@@ -24,7 +26,7 @@ def get_posting_list(dictionary, postings, term):
 def usage():
     print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
 
-def run_search(dict_file, postings_file, queries_file, results_file):
+def run_search(dict_file, postings_file, query_file, results_file):
     """
     Using the given dictionary file and postings file, perform searching on the given queries file
     and output the results to a file
@@ -45,9 +47,21 @@ def run_search(dict_file, postings_file, queries_file, results_file):
 
     print(get_posting_list(dictionary, postings, 'yong'))
 
+    with open(query_file) as f:
+        count = 0
+        lst_of_relevant_docs = []
+
+        for line in f:
+            if count == 0: # first line
+                query = line
+            else:
+                lst_of_relevant_docs.append(int(line))
+            count += 1
+
+        query_details = QueryDetails(query, lst_of_relevant_docs)
 
 
-dictionary_file = postings_file = file_of_queries = output_file_of_results = None
+dictionary_file = postings_file = query_file = output_file_of_results = None
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'd:p:q:o:')
@@ -61,14 +75,14 @@ for o, a in opts:
     elif o == '-p':
         postings_file = a
     elif o == '-q':
-        file_of_queries = a
+        query_file = a
     elif o == '-o':
         file_of_output = a
     else:
         assert False, "unhandled option"
 
-if dictionary_file == None or postings_file == None or file_of_queries == None or file_of_output == None :
+if dictionary_file == None or postings_file == None or query_file == None or file_of_output == None :
     usage()
     sys.exit(2)
 
-run_search(dictionary_file, postings_file, file_of_queries, file_of_output)
+run_search(dictionary_file, postings_file, query_file, file_of_output)
